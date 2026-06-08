c57dc19700476170911640d1bb6c0d6025d9fd08
songsuyoung <tndud3999@naver.com>
2026-06-08 21:48:35 +0900

    컴파일 오류 수정 : PathPoint Type 제거 목표지점 및 시작점 데이터 테이블화 시킬 예정. 뿐만아니라 볼륨을 두어서 부딪힐 경우 다시 재 판단하도록 할 예정 예) 운동장 -> 운동한다/안한다 스탯으로 결정 ... 등

--- Source/Resonance/Data/RRegionDataTable.h
+++ Source/Resonance/Data/RRegionDataTable.h
@@ -8,19 +8,11 @@ struct RESONANCE_API FRRegionDataTable : public FTableRowBase
 {
 	GENERATED_BODY()
 public:
-	virtual void OnPostDataImport(const UDataTable* InDataTable, const FName InRowName, TArray<FString>& OutCollectedImportProblems) override;
-	virtual void OnDataTableChanged(const UDataTable* InDataTable, const FName InRowName) override;
-	
-	/*// 장소 이름
-	UPROPERTY(EditAnywhere, Category = "")
+	// 장소 이름
+	UPROPERTY(EditDefaultsOnly, Category = "Region|Settings")
 	FString RegionName;
-	
+
 	// 장소 위치 (스폰/목표 지점 위치)
-	UPROPERTY(EditAnywhere, Cetegory = "")
-	FVector Location;*/
-	
-private:
-	
-	UPROPERTY(Transient)
-	int32 ID;
+	UPROPERTY(EditDefaultsOnly, Category = "Region|Settings")
+	FVector Location;
 };
--- Source/ResonanceCore/Data/RCoreEnums.h
+++ Source/ResonanceCore/Data/RCoreEnums.h
@@ -7,16 +7,3 @@ enum class ERBakeType : uint8
 	WarpPoint,
 	PathPoint,
 };
-
-
-UENUM(BlueprintType)
-enum class ERPathPointType : uint8
-{
-	Spawn,       // NPC 스폰 지점
-	Destination, // 목적지
-	Waypoint,    // 경유지
-	Walk,        // 걷는 구간
-	Run,         // 뛰는 구간
-	RightPathOn, // 우측 경로 시작
-	RightPathOff, // 우측 경로 끝 -> 원래 길찾기 복귀
-};
